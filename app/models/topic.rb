d4d425559910c92621d5d023745c43df86c182cc
Bill <bill@pcrsocial.com>
2017-08-25 13:47:16 -0400

    assignment 22 done

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,10 @@
 class Topic < ApplicationRecord
 has_many :posts , dependent: :destroy
 has_many :sponsoredposts 
+
+   validates :name, length: { minimum: 5 }, presence: true
+   validates :description, length: { minimum: 15 }, presence: true
+   validates :topic, presence: true
+
+
 end
